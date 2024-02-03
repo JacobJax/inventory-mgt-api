64afa1864bbd5af8f90c8dc19d46cff2c3fdebb3
JacobJax <owinodav99@gmail.com>
2024-02-03 13:17:29 +0300

    disable es-lint max length

--- functions/index.js
+++ functions/index.js
@@ -39,7 +39,6 @@ app.post("/", async (req, res) => {
 
 app.route("/:id")
     .get(async (req, res) => {
-      // eslint-disable-next-line max-len
       const snapshot = await admin.firestore().collection("inventory").doc(req.params.id).get();
       const itemId = snapshot.id;
       const itemData = snapshot.data();
@@ -47,30 +46,22 @@ app.route("/:id")
       res.status(200).send(JSON.stringify({itemId, ...itemData}));
     })
     .put(async (req, res) => {
-      // eslint-disable-next-line max-len
       const snapshot = await admin.firestore().collection("inventory").doc(req.params.id).get();
       const data = snapshot.data();
 
       const postBody = {
-        // eslint-disable-next-line max-len
         "storage_shelf_column_number": req.body.storage_shelf_column_number ? req.body.storage_shelf_column_number : data.storage_shelf_column_number,
         "quantity": req.body.quantity ? req.body.quantity : data.quantity,
-        // eslint-disable-next-line max-len
         "shop_price": req.body.shop_price ? req.body.shop_price : data.shop_price,
-        // eslint-disable-next-line max-len
         "shop_billing_type": req.body.shop_billing_type ? req.body.shop_billing_type : data.shop_billing_type,
         "name": req.body.name ? req.body.name : data.name,
         "slug": req.body.slug ? req.body.slug : data.slug,
-        // eslint-disable-next-line max-len
         "storage_shelf_row_number": req.body.storage_shelf_row_number ? req.body.storage_shelf_row_number : data.storage_shelf_row_number,
-        // eslint-disable-next-line max-len
         "storage_quantification_type": req.body.storage_quantification_type ? req.body.storage_quantification_type : data.storage_quantification_type,
       };
 
-      // eslint-disable-next-line max-len
       await admin.firestore().collection("inventory").doc(req.params.id).update(postBody);
 
-      // eslint-disable-next-line max-len
       res.status(200).send(JSON.stringify({"message": "Item updated successfully"}));
     })
     .delete(async (req, res) => {
@@ -78,12 +69,4 @@ app.route("/:id")
       res.status(200).send(JSON.stringify({"message": "Item deleted succesfully"}));
     });
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
 exports.inventory = onRequest(app);
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
