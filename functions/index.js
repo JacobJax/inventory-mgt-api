7390951863c89e8742736963b4b4d54bd9e48b2f
JacobJax <owinodav99@gmail.com>
2024-02-04 12:45:19 +0300

    implement error handling and input validation

--- functions/index.js
+++ functions/index.js
@@ -11,62 +11,112 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const express = require("express");
 
+const {addItemValidation, updateItemValidation} = require("./validators/itemsValidator");
+
+
 const app = express();
 const admin = require("firebase-admin");
 admin.initializeApp();
 
 app.get("/", async (req, res) => {
-  const snapshot = await admin.firestore().collection("inventory").get();
+  try {
+    const snapshot = await admin.firestore().collection("inventory").get();
 
-  const items = [];
-  snapshot.forEach((doc) => {
-    const id = doc.id;
-    const data = doc.data();
+    const items = [];
+    snapshot.forEach((doc) => {
+      const id = doc.id;
+      const data = doc.data();
 
-    items.push({id, ...data});
-  });
+      items.push({id, ...data});
+    });
 
-  res.status(200).send(JSON.stringify(items));
+    res.status(200).send(JSON.stringify(items));
+  } catch (error) {
+    res.status(500).send(JSON.stringify({"error": error}));
+  }
 });
 
 app.post("/", async (req, res) => {
   const item = req.body;
 
-  await admin.firestore().collection("inventory").add(item);
+  // validate user input
+  const {error} = addItemValidation(item);
+  if (error) return res.status(400).json(error.details[0].message);
 
-  res.status(201).send(JSON.stringify(item));
+  try {
+    await admin.firestore().collection("inventory").add(item);
+    res.status(201).send(JSON.stringify(item));
+  } catch (error) {
+    res.status(500).send(JSON.stringify({"error": error}));
+  }
 });
 
 app.route("/:id")
     .get(async (req, res) => {
-      const snapshot = await admin.firestore().collection("inventory").doc(req.params.id).get();
-      const itemId = snapshot.id;
-      const itemData = snapshot.data();
+      try {
+        const snapshot = await admin.firestore().collection("inventory").doc(req.params.id).get();
 
-      res.status(200).send(JSON.stringify({itemId, ...itemData}));
+        // return error if item is not found
+        if (!snapshot.exists) {
+          return res.status(404).send(JSON.stringify({"error": "item not found"}));
+        }
+
+        // create item format
+        const itemId = snapshot.id;
+        const itemData = snapshot.data();
+
+        res.status(200).send(JSON.stringify({itemId, ...itemData}));
+      } catch (error) {
+        res.status(500).send(JSON.stringify({"error": error}));
+      }
     })
     .put(async (req, res) => {
-      const snapshot = await admin.firestore().collection("inventory").doc(req.params.id).get();
-      const data = snapshot.data();
+      try {
+        const snapshot = await admin.firestore().collection("inventory").doc(req.params.id).get();
+        // return error if item is not found
+        if (!snapshot.exists) {
+          return res.status(404).send(JSON.stringify({"error": "item not found"}));
+        }
+        const data = snapshot.data();
 
-      const postBody = {
-        "storage_shelf_column_number": req.body.storage_shelf_column_number ? req.body.storage_shelf_column_number : data.storage_shelf_column_number,
-        "quantity": req.body.quantity ? req.body.quantity : data.quantity,
-        "shop_price": req.body.shop_price ? req.body.shop_price : data.shop_price,
-        "shop_billing_type": req.body.shop_billing_type ? req.body.shop_billing_type : data.shop_billing_type,
-        "name": req.body.name ? req.body.name : data.name,
-        "slug": req.body.slug ? req.body.slug : data.slug,
-        "storage_shelf_row_number": req.body.storage_shelf_row_number ? req.body.storage_shelf_row_number : data.storage_shelf_row_number,
-        "storage_quantification_type": req.body.storage_quantification_type ? req.body.storage_quantification_type : data.storage_quantification_type,
-      };
+        // validate user input
+        const {error} = updateItemValidation(req.body);
+        if (error) return res.status(400).json(error.details[0].message);
 
-      await admin.firestore().collection("inventory").doc(req.params.id).update(postBody);
+        const postBody = {
+          "storage_shelf_column_number": req.body.storage_shelf_column_number ? req.body.storage_shelf_column_number : data.storage_shelf_column_number,
+          "quantity": req.body.quantity ? req.body.quantity : data.quantity,
+          "shop_price": req.body.shop_price ? req.body.shop_price : data.shop_price,
+          "shop_billing_type": req.body.shop_billing_type ? req.body.shop_billing_type : data.shop_billing_type,
+          "name": req.body.name ? req.body.name : data.name,
+          "slug": req.body.slug ? req.body.slug : data.slug,
+          "storage_shelf_row_number": req.body.storage_shelf_row_number ? req.body.storage_shelf_row_number : data.storage_shelf_row_number,
+          "storage_quantification_type": req.body.storage_quantification_type ? req.body.storage_quantification_type : data.storage_quantification_type,
+        };
 
-      res.status(200).send(JSON.stringify({"message": "Item updated successfully"}));
+        await admin.firestore().collection("inventory").doc(req.params.id).update(postBody);
+
+        res.status(200).send(JSON.stringify({"message": "Item updated successfully"}));
+      } catch (error) {
+        res.status(500).send(JSON.stringify({"error": error}));
+      }
     })
     .delete(async (req, res) => {
-      await admin.firestore().collection("inventory").doc(req.params.id).delete();
-      res.status(200).send(JSON.stringify({"message": "Item deleted succesfully"}));
+      try {
+        // Get item from database
+        const snapshot = await admin.firestore().collection("inventory").doc(req.params.id).get();
+
+        // return error if item is not found
+        if (!snapshot.exists) {
+          return res.status(404).send(JSON.stringify({"error": "item not found"}));
+        }
+
+        // delete item
+        await admin.firestore().collection("inventory").doc(req.params.id).delete();
+        res.status(200).send(JSON.stringify({"message": "Item deleted succesfully"}));
+      } catch (error) {
+        res.status(500).send(JSON.stringify({"error": error}));
+      }
     });
 
 exports.inventory = onRequest(app);
